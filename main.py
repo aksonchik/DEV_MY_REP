b25250c97a552e4ea686365887d66a227f8b9727
aksonchik <as6664660@gmail.com>
2024-11-27 21:21:13 +0300

    Домашка 8 тема

--- main.py
+++ main.py
@@ -1,62 +1,108 @@
-import time
-from functools import wraps, reduce
-
 # Задание 1
-numbers = [1, 2, 3]
+def calculate_bmi():
+    try:
+        # Запросить у пользователя информацию о росте в сантиметрах
+        height_cm = float(input("Введите свой рост в сантиметрах: "))
+        # Перевод высоты в метры
+        height_m = height_cm / 100
+        # Запросить у пользователя вес в килограммах
+        weight_kg = float(input("Введите свой вес в килограммах: "))
 
-string_list = list(map(str, numbers))
+        # Рассчет ИМТ
+        bmi = weight_kg / (height_m ** 2)
 
-print(string_list)
+        # Определите категорию ИМТ
+        if bmi < 18.5:
+            category = "Недостаточный вес"
+        elif 18.5 <= bmi < 24.9:
+            category = "Нормальный вес"
+        elif 25 <= bmi < 29.9:
+            category = "Избыточный вес"
+        else:
+            category = "Ожирение"
+
+        # Отображение результатов
+        print(f"Ваш ИМТ равен {bmi:.2f}, который классифицируется как {category}.")
+
+    except ValueError:
+        print("Неверный ввод. Пожалуйста, введите числовые значения для роста и веса.")
+
+
+calculate_bmi()
+
 
 # Задание 2
-numbers = [1, -2, 3, 0, -5]
-
-filtered_numbers = list(filter(lambda x: x > 0, numbers))
-
-print(filtered_numbers)
-
-# Задание 3
-strings = ['abc', 'abcba', '12321', 'hello', 'level']
-
-palindromes = list(filter(lambda s: s == s[::-1], strings))
-
-print(palindromes)
+def add(a, b):
+    return a + b
 
 
-# Задание 4
-def timing_decorator(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"Функция {func.__name__} заняла {elapsed_time:.6f} секунд для выполнения.")
-        return result
-
-    return wrapper
+def subtract(a, b):
+    return a - b
 
 
-@timing_decorator
-def example_function(n):
-    sum = 0
-    for i in range(n):
-        sum += i
-    return sum
+def multiply(a, b):
+    return a * b
 
 
-example_function(1000000)
+def divide(a, b):
+    return a / b
 
-# Задание 5
-rooms = [
-    {"name": "Kitchen", "length": 6, "width": 4},
-    {"name": "Room 1", "length": 5.5, "width": 4.5},
-    {"name": "Room 2", "length": 5, "width": 4},
-    {"name": "Room 3", "length": 7, "width": 6.3},
-]
 
-areas = map(lambda room: room["length"] * room["width"], rooms)
+def exponentiate(a, b):
+    return a ** b
 
-total_area = reduce(lambda x, y: x + y, areas)
 
-print(f"Площадь: {total_area} квадратных метров.")
+def modulo(a, b):
+    return a % b
+
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+    '**': exponentiate,
+    '%': modulo
+}
+
+
+def calculator():
+    while True:
+        print("Добро пожаловать в программу Калькулятор!")
+        print("Доступные операции: +, -, *, /, **, %")
+
+        while True:
+            try:
+                num1 = float(input("Введите первое число: "))
+                break
+            except ValueError:
+                print("Неверный ввод. Пожалуйста, введите числовое значение.")
+
+        while True:
+            try:
+                num2 = float(input("Введите второе число: "))
+                break
+            except ValueError:
+                print("Неверный ввод. Пожалуйста, введите числовое значение.")
+
+        operation = input("Введите к операции (+, -, *, /, **, %): ")
+        if operation not in operations:
+            print("Недопустимая операция. Пожалуйста, выберите одну из доступных операций.")
+            continue
+
+        try:
+            if operation == '/' and num2 == 0:
+                print("Ошибка: Деление на ноль недопустимо.")
+            else:
+                result = operations[operation](num1, num2)
+                print(f"Результат: {num1} {operation} {num2} = {result}")
+        except ZeroDivisionError:
+            print("Ошибка: Деление на ноль недопустимо.")
+
+        choice = input("Вы хотите выполнить другой расчет? (да/нет): ")
+        if choice.lower() != 'да':
+            print("Благодарим вас за использование калькулятора. До свидания!")
+            break
+
+
+calculator()
